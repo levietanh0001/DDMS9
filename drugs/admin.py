a9a2dbb0c3006a25f0566929dea8e38e2daef00a
levietanh_bi9035 <anhlv.bi9035@st.usth.edu.vn>
2021-10-28 19:05:03 +0700

    admin import csv

--- drugs/admin.py
+++ drugs/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
+from django.core.checks import messages
+from django.http.response import HttpResponseRedirect
+from django.urls import path
+from django.shortcuts import render, redirect
+from django import forms
+# local
 from .models import Drug
 
-
+class CsvImportForm(forms.Form):
+    csv_file = forms.FileField()
+    
+    
 class DrugAdmin(admin.ModelAdmin):
     list_display = [
         'drug_id',
@@ -14,6 +23,39 @@ class DrugAdmin(admin.ModelAdmin):
         'description',
         'updated_date',
     ]
-    
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('upload-csv/', self.upload_csv),
+        ]
+        return my_urls + urls
+    def upload_csv(self, request):
+        if request.method == "POST":
+            csv_file = request.FILES["csv_file"]
+            if not csv_file.name.endswith('.csv'):
+                messages.warning(request, 'Only CSV files are accepted!')
+                return HttpResponseRedirect(request.path_info)
+            
+            
+            f = csv_file.read().decode('utf-8')
+            csv_data = f.split("\n")
+            for x in csv_data:
+                fields = x.split(",")
+                created = Drug.objects.update_or_create(
+                    drug_id = fields[0],
+                    name = fields[1],
+                    drug_type = fields[2],
+                    amount = fields[3],
+                    exp = fields[4],
+                    mfg = fields[5],
+                    brand = fields[6],
+                    description = fields[7],
+                    updated_date = fields[8],
+                )
+            self.message_user(request, "Your csv file has been uploaded!")
+            return redirect("..")
+        form = CsvImportForm()
+        payload = {"form": form}
+        return render(request, "admin/csv_upload.html", payload)
     
 admin.site.register(Drug, DrugAdmin)
